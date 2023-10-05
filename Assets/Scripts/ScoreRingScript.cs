6fbd929613b8c8e6cb4dcb086e91a4f7ed9e09bd
jgonzale5 <60185919+jgonzale5@users.noreply.github.com>
2023-10-05 15:15:33 -0400

    Added sound, music, and particle effects

--- Assets/Scripts/ScoreRingScript.cs
+++ Assets/Scripts/ScoreRingScript.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class ScoreRingScript : MonoBehaviour
 {
+    [Header("Collision Detection")]
     //The tag that must ne on the other object of the trigger interaction for
     //points to be added.
     public string scoringTag = "Player";
+
+    [Header("Particle Effect")]
+    //The prefab of the particle effect that's gonna play when this ring is scored
+    public Transform ringExplosion;
+
+    [Header("Faking Destruction")]
+    //The mesh of the ring, so that we can hide it
+    public GameObject ringMesh;
+    //The collider of the ring, so we can disable it
+    public Collider ringCollider;
+
+    [Header("Sound")]
+    //The audio source that will play the scoring sound
+    public AudioSource ringSoundPlayer;
+    //The sound that will be played when points are scored with this ring
+    public AudioClip scoreSound;
+
+    private void Start()
+    {
+        //We tell the audio source what clip it will play
+        ringSoundPlayer.clip = scoreSound;
+    }
 
     //This callback function will be called when a collider on the same object
     //as this script enters a trigger (or if there is a trigger on this object
@@ -22,8 +45,23 @@ public class ScoreRingScript : MonoBehaviour
             //Add 10 points to score manager, which will display the new score
             ScoreManager.Instance.AddScore(10);
 
+            //Spawn a ring explosion at the position of this object, with its
+            //same rotation.
+            Instantiate(ringExplosion, 
+                this.transform.position, 
+                this.transform.rotation,
+                this.transform);
+
             //We destroy this object
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+
+            //Instead of destroying this object, we're going to hide the ring
+            //and the collider so the player thinks it's destroyed.
+            ringMesh.SetActive(false);
+            ringCollider.enabled = false;
+
+            //Tell the player to play the sound assigned to its "clip" variable
+            ringSoundPlayer.Play();
         }
     }
 }
